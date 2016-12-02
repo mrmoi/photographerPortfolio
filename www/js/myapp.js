57a56136a820f44ebac21ad6d59c3a8efc3f11fa
Moises Martinez <mlmart16@asu.edu>
2016-12-01 18:08:44 -0700

    setup database firebase connection and form

---
+++ www/js/myapp.js
@@ -0,0 +1,38 @@
+/**
+ * Created by miniMoimartz on 12/1/16.
+ */
+
+var myApp = angular.module('myApp', ['ui.router', 'firebase'])
+    .constant('FIREBASE_URL', 'https://germanproductions.firebaseio.com/');
+
+
+myApp.config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('home');
+
+    var states = [
+
+        {
+            name: 'home',
+            url: '/home',
+            templateUrl: 'partials/home.html',
+            controller: 'dbController'
+        },
+
+        {
+            name: 'control',
+            url: '/control',
+            templateUrl: 'partials/control.html',
+            controller: 'dbController'
+        }
+    ]
+
+    states.forEach(function (state) {
+        $stateProvider.state(state);
+    });
+
+
+});
+
+
+
---
+++ www/js/services/databaseService.js
@@ -0,0 +1,64 @@
+/**
+ * Created by miniMoimartz on 12/1/16.
+ */
+myApp.controller('dbController',['$scope','$rootScope','$firebaseAuth','$firebaseArray','FIREBASE_URL',
+    function($scope, $rootScope, $firebaseAuth, $firebaseArray, FIREBASE_URL) {
+
+
+        /* NAME FIELD*/
+        var nameRef = new Firebase(FIREBASE_URL + '/name');
+        var nameInfo = $firebaseArray(nameRef);
+        $scope.names = nameInfo;
+
+        /* ABOUT ME CONTENT */
+        var bioRef = new Firebase(FIREBASE_URL + '/bio');
+        var bioInfo = new $firebaseArray(bioRef);
+        var index = bioInfo.$indexFor(bioRef);
+        $scope.bios = bioInfo;
+
+
+        /* NAME FIELD FUNCTION */
+        $scope.addName = function() {
+            nameInfo.$add({
+                name: $scope.nameField,
+                date: Firebase.ServerValue.TIMESTAMP
+            }).then(function () {
+                $scope.nameField = '';
+            });
+        };
+
+        /* ABOUT ME CONTENT FUNCTION */
+        $scope.addBio = function () {
+            bioInfo.$add({
+                about: $scope.aboutField,
+                date: Firebase.ServerValue.TIMESTAMP
+            }).then(function() {
+                $scope.aboutField='';
+            });
+        };
+
+/*
+        /!* DELETE BIO *!/
+        $scope.deleteBio = function() {
+            //bioInfo.$remove();
+
+            bioInfo.$remove(index + 1).then(function(ref) {
+                // data has been deleted locally and in the database
+            }, function(error) {
+                console.log("Error:", error);
+            });
+
+            console.log("remove");
+        };
+*/
+
+
+
+
+
+
+
+    }]); // END Controller
+
+
+
