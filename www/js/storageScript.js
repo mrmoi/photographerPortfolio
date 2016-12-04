2e783a550a31c5e5518d6eb8a891410fce40e0f4
Moises Martinez <mlmart16@asu.edu>
2016-12-03 17:17:55 -0700

    storage upload only in index.html

---
+++ www/js/storageScript.js
@@ -0,0 +1,42 @@
+/**
+ * Created by miniMoimartz on 12/3/16.
+ */
+/*
+        //var uploader = document.getElementById('uploader');
+        fileButton = document.getElementById('fileButton');
+
+        console.log('got buttons');
+
+        fileButton.addEventListener('change', function (e) {
+
+        console.log('getting file..');
+
+        // Get file
+        var file = e.target.files[0];
+
+        // Create storage ref
+        var storageRef = firebase.storage().ref('pictures/' + file.name);
+
+        // Upload File
+        var task = storageRef.put(file);
+
+        // Update the progress bar
+        task.on('state_changed',
+            //function progress(snapshot) {
+              //  var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                //uploader.value = percentage;
+                //console.log('im alive..');
+            //},
+            function error(err) {
+                console.log('Error');
+            },
+            function complete() {
+                console.log('Completed uploading file');
+            }
+        );
+    });*/
+
+
+
+
+
